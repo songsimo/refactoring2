328fed5aada81e0f750c23c211789749abc33646
songsimo <chy.song89@gmail.com>
2025-01-19 22:58:44 +0900

    doc: README.md 추가

--- src/test/ch01/refactor/StatementTest.java
+++ src/test/ch01/refactor/StatementTest.java
@@ -17,13 +17,6 @@ import java.util.List;
 import static org.assertj.core.api.Assertions.assertThat;
 
 class StatementTest {
-    public final String EXPECTED_INVOICE_MESSAGE = "청구 내역 (고객명: BigCo)\n" +
-            "  Hamlet: $650.00 (55석)\n" +
-            "  As You Like It: $580.00 (35석)\n" +
-            "  Othello: $500.00 (40석)\n" +
-            "총액: $1,730.00\n" +
-            "적립 포인트: 47점\n";
-
     private static Invoice invoice;
     private static Plays plays;
 
